635691352db3f0efd1de0c5ab1859910dde1766f
Mohamed Elshahawi <contact@elshahawi.dev>
2025-11-02 20:21:45 +0000

    change mode for scripts and use db and s3 for mlflow

--- scripts/mlflow_ui.sh
+++ scripts/mlflow_ui.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 source venv/bin/activate
 
 mlflow server \
-  --backend-store-uri ./mlruns \
-  --artifacts-destination ./mlartifacts \
+  --backend-store-uri sqlite:///mlflow.db \
+  --artifacts-destination s3://my-ml-flow-artifacts \
   --serve-artifacts \
   --host 0.0.0.0 --port 5000
 
